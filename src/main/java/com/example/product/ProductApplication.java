4d559049b803ebee651ae4b7c4a7c7da91919145
Andana Paringga <andana.paringga@spesolution.com>
2022-05-31 15:15:38 +0700

    5 commit

--- src/main/java/com/example/product/ProductApplication.java
+++ src/main/java/com/example/product/ProductApplication.java
@@ -1,12 +1,9 @@
 package com.example.product;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class ProductApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/product/model/Product.java
+++ src/main/java/com/example/product/model/Product.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document (collection = "ProductDB")
+@Document (collection = "book")
 public class Product {
 	
 	@Id
